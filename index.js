45b6469c65d4b5347b532287241e815ca85f4df3
Nick Olds <nolds9@yahoo.com>
2016-04-05 22:06:36 -0400

    express rendered views with hbs

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var hbs     = require("express-handlebars");
 
 var app     = express();
 
-app.get("/", function index (req, res){
-  res.send("Jello World!");
+// Configure Handlebars to our app's conventions
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
 });
 
 app.listen(3001, function onPort (){
